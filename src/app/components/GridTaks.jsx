8ed700d8fc625f82e23cc028999e033c39d98d7b
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-03-06 08:25:34 -0600

    Add component NavCategories

--- src/app/components/GridTaks.jsx
+++ src/app/components/GridTaks.jsx
@@ -1,43 +1,35 @@
 'use client'
 
-import {Card, CardHeader, CardBody, CardFooter, Spinner, Button} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Button, Spinner} from "@nextui-org/react";
 import { FaClock } from "react-icons/fa";
 import { formatDate } from "../utils/funcionts";
 import { useTaskContext } from "../provider/TasksProvider";
+import NavCategories from "./NavCategories";
 
 export default function GridTaks() {
-  
+
   const {taks, categories, load} = useTaskContext();
   
   return (
     <>
       {
-        taks.length == 0 && 
-        <div className="flex justify-center w-full p-xl-0">
-            {
-              load &&
-              <div className="flex items-center">
-                <Spinner size="lg" label="Cargando..." color="primary" />
-              </div>
-            }
+          taks.length == 0 && 
+          <div className="flex justify-center w-full p-xl-0">
+              {
+                load &&
+                <div className="flex items-center">
+                  <Spinner size="lg" label="Cargando..." color="primary" />
+                </div>
+              }
 
-            {
-              (taks.length == 0 && !load) && 
-              <h3 className="font-semibold text-2xl">No hay tareas registradas</h3>
-            }
-        </div>
-      }
-
-      <div className="flex gap-x-3 flex-row mb-6">
-        {
-          categories.map((category, index) => (
-            <Button size="md" key={category + index} radius="full" className="flex flex-row items-center justify-between">
-              {category.category ? category.category : "Sin categoria"}
-              <span className="bg-gray-500 h-5 w-5 rounded-full text-small">{category.count}</span>
-            </Button>
-          ))
+              {
+                (taks.length == 0 && !load) && 
+                <h3 className="font-semibold text-2xl">No hay tareas registradas</h3>
+              }
+          </div>
         }
-      </div>
+
+      <NavCategories/>
 
      <div className="grid grid-cols-12 gap-6">
         {
---
+++ src/app/components/NavCategories.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Button} from "@nextui-org/react";
+import { useTaskContext } from "../provider/TasksProvider";
+
+export default function NavCategories() {
+
+  const {categories} = useTaskContext();
+
+  return (
+    <nav className='flex gap-x-3 flex-row mb-6'>
+      {
+        categories.map((category, index) => (
+          <Button size="md" key={category + index} radius="full" className="flex flex-row items-center justify-between">
+            {category.category ? category.category : "Sin categoria"}
+            <span className="bg-gray-500 h-5 w-5 rounded-full text-small">{category.count}</span>
+          </Button>
+        ))
+      }
+    </nav>
+  )
+}
