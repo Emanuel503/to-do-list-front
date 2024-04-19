34a24a71dc6f317723ebada5930f91c3d957feba
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-04-19 10:59:06 -0600

    Add react hook form

--- src/app/components/ModalTask.jsx
+++ src/app/components/ModalTask.jsx
@@ -4,11 +4,21 @@ import React from 'react'
 import { FiPlus } from "react-icons/fi";
 import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Textarea, Input, AutocompleteItem, Autocomplete} from "@nextui-org/react";
 import { useTaskContext } from '../provider/TasksProvider';
+import { useForm } from 'react-hook-form';
 
 export default function ModalTask() {
   const {isOpen, onOpen, onClose} = useDisclosure();
 
   const {categories} = useTaskContext();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => {console.log(data)}
 
   return (
     <>
@@ -25,36 +35,48 @@ export default function ModalTask() {
           placement="top-center"
       >
           <ModalContent>
-          {(onClose) => (
-              <>
-              <ModalHeader className="flex flex-col gap-1">Registrar tarea</ModalHeader>
-              <ModalBody>
-                <form className=' flex flex-col gap-y-3'>  
-                  <Input type="text" label="Titulo" name="title" placeholder="Escribe el titulo" />
-                  <Textarea
-                      label="Descripcion"
-                      placeholder="Escribe una descripcion"
-                  />
+            {(onClose) => (
+                <>
+                <ModalHeader className="flex flex-col gap-1">Registrar tarea</ModalHeader>
+                <form onSubmit={handleSubmit(onSubmit)} className=' flex flex-col gap-y-3'>  
+                  <ModalBody>
+                      <Input 
+                        type="text" 
+                        label="Titulo" 
+                        name="title" 
+                        placeholder="Escribe el titulo" 
+                        {...register("title")} 
+                      />
 
-                  <Autocomplete  
-                    label="Categoria" 
-                    allowsCustomValue={true}
-                    defaultItems={categories}
-                  >
-                    {(item) => <AutocompleteItem key={item.category}>{item.category}</AutocompleteItem>}
-                  </Autocomplete>
+                      <Textarea
+                          label="Descripcion"
+                          placeholder="Escribe una descripcion"
+                          {...register("description", {required: true})}
+                          color={`${errors?.description?.type === "required" ? 'danger': 'default'}`}
+                      />
+                      {errors?.description?.type === "required" && <p className='text-red-700 text-sm'>Este campo es requerido</p>}
+
+                      <Autocomplete  
+                        label="Categoria" 
+                        allowsCustomValue={true}
+                        defaultItems={categories}
+                        {...register("category")}
+                      >
+                        {(item) => <AutocompleteItem key={item.category}>{item.category ? item.category : "Sin categoria"}</AutocompleteItem>}
+                      </Autocomplete>
+                    
+                  </ModalBody>
+                  <ModalFooter>
+                      <Button onPress={onClose}>
+                        Cerrar
+                      </Button>
+                      <Button type='submit' color="primary">
+                        Guardar
+                      </Button>
+                  </ModalFooter>
                 </form>
-              </ModalBody>
-              <ModalFooter>
-                  <Button onPress={onClose}>
-                  Cerrar
-                  </Button>
-                  <Button color="primary" onPress={onClose}>
-                  Guardar
-                  </Button>
-              </ModalFooter>
-              </>
-          )}
+                </>
+            )}
           </ModalContent>
       </Modal>
     </>
